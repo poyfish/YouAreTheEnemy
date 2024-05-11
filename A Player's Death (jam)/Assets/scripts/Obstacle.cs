a69961f22e61bb93685b3dbe42adce281d4623e1
poyfish <125211156+poyfish@users.noreply.github.com>
2024-05-11 12:31:10 +0300

    added walking animation option to obstacle script

--- A Player's Death (jam)/Assets/scripts/Obstacle.cs
+++ A Player's Death (jam)/Assets/scripts/Obstacle.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
@@ -8,8 +9,12 @@ public class Obstacle : MonoBehaviour
     private Animator anim;
     BoxCollider2D coll;
 
+    public bool doesUseWalkingAnimation;
+
     public string startingAnimationName;
     public string PlacedAnimationName;
+
+    public string walkingAnimationName;
 
     public bool isReady;
 
@@ -21,6 +26,14 @@ public class Obstacle : MonoBehaviour
         anim.CrossFade(PlacedAnimationName, 0, 0);
 
         coll = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (isReady && doesUseWalkingAnimation)
+        {
+            anim.CrossFade(walkingAnimationName, 0, 0);
+        }
     }
 
     void StartSpike()
